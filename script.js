862fad0a6c3f0a773d8a6e089b66d4a9c274ef5f
darllenwr <113836402+darllenwr@users.noreply.github.com>
2023-08-16 19:26:07 +0100

    Added key files

---
+++ script.js
@@ -0,0 +1,11 @@
+const buttonSubmit = document.querySelector("#btn-submit");
+
+
+
+buttonSubmit.addEventListener("submit", event => {
+    event.preventDefault();
+                           console.log("What happened?");      
+ 
+                              })
+
+
