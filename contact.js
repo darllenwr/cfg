de203f1a3615e64c0988cfd6d7eb2746bf7168db
darllenwr <113836402+darllenwr@users.noreply.github.com>
2023-08-22 10:05:24 +0100

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,21 @@
+// Creates a variable from the submit button on the form //
+
+const buttonSubmit = document.querySelector("#btn-submit");
+
+const submitForm = () => {
+  buttonSubmit.addEventListener("submit", event => {
+   event.preventDefault();    
+ });    
+}
+   
+
+
+
+
+  
+
+
+
+      
+       
+  
