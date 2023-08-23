8b7e7eb22cc31d2f4d392510c76864f4af1f9f29
darllenwr <113836402+darllenwr@users.noreply.github.com>
2023-08-23 15:25:42 +0100

    Update contact.js
    JavaScript update

--- contact.js
+++ contact.js
@@ -2,11 +2,16 @@
 
 const buttonSubmit = document.querySelector("#btn-submit");
 
+// Some of the world's most pointless JavaScript. Sends people to a 'submission successful' page on click. It should have been done via HTML, but I wanted to get some JavaScript in.//
+
 const submitForm = () => {
-  buttonSubmit.addEventListener("submit", event => {
-   event.preventDefault();    
+  buttonSubmit.addEventListener("click", () => {
+    document.location.href ="contact-submit.html";
  });    
 }
+   
+submitForm()
+
    
 
 
